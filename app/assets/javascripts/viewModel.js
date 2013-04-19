188c27cf6563b733a4766fe61bad33a66866ffb0
Devin <devinsmoss@gmail.com>
2013-04-19 15:57:49 -0500

    Updated viewModel.js

--- app/assets/javascripts/viewModel.js
+++ app/assets/javascripts/viewModel.js
@@ -1,41 +1,42 @@
 $(document).ready(function() {
-  var BaseViewModel = Object.extend({
-    init: function() {
-      var self = this;
-      self.updateViewModel = function(name) {
-        ko.applyBindings(eval('new ' + name + 'ViewModel()'));
-      }
-    }
-  });
-
-  var HomeViewModel = BaseViewModel.extend({
-    init: function() {
-      var self = this;
-      self.templateName = 'home';
-    }
-  });
-  /*function HomeViewModel() {
-    var self = this;
-    self.templateName = 'home';
-  }*/
-
-  window.blogApp = {};
-  window.blogApp.viewModel = new HomeViewModel();
-  ko.applyBindings(window.blogApp.viewModel);
-
-  window.blogApp.updateViewModel = function(name) {
-    var viewModel = eval('new ' + name + 'ViewModel()');
-    window.blogApp.viewModel = viewModel;
-    ko.applyBindings(window.blogApp.viewModel);
-  };
-
-  function PostsViewModel() {
-    var self = this;
-    self.templateName = 'posts';
+  function BaseViewModel(tName) {
+  	this.templateName = tName;
   }
+  //Base class for all viewmodels
+  //Handles switching of template on page
+  BaseViewModel.prototype = {
+  	updateViewModel : function(templateName, options) {
+  		ko.applyBindings(new this.viewModels[templateName](options));//viewModels is a container attached to the prototype (this)
+  	}
+  };
+  
+  //Initialize viewModels container
+  BaseViewModel.prototype.viewModels = { "Home" : HomeViewModel, "Posts" : PostsViewModel, "Post" : PostViewModel};
+
+  ko.applyBindings(new HomeViewModel());
 });
 
-/*function PostsViewModel() {
-  var self = this;
-  self.templateName = 'posts';
-}*/
+  function PostsViewModel() {
+  	var self = this;
+  	BaseViewModel.call(self, "Posts");
+  	
+  }
+  PostsViewModel.prototype = Object.create(BaseViewModel.prototype);
+
+  function PostViewModel(post) {
+  	var self = this;
+  	BaseViewModel.call(self, "Post");
+  	self.post = new Post();
+  }
+  PostViewModel.prototype = Object.create(BaseViewModel.prototype);
+
+  function HomeViewModel() {
+  	var self = this;
+  	BaseViewModel.call(self, "Home");
+  }
+  HomeViewModel.prototype = Object.create(BaseViewModel.prototype);
+
+function Post(post) {
+	var self = this;
+	this.id = post.id;
+}
